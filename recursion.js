89ef9b1591fb11048c54ffe843ece7dfd0d2dc44
Christina Labay <Christina.m.labay@gmail.com>
2019-07-09 20:03:08 -0500

    triangle number

--- recursion.js
+++ recursion.js
@@ -43,3 +43,21 @@ reverseString = (x) =>{
   return reverseString(x.slice(1)) + x.charAt(0)
 }
 console.log(reverseString('myAwesomeString'))
+
+//nth Triangular Number
+//Calculate the nth triangular number. 
+//A triangular number counts the objects that can form an equilateral triangle. 
+//The nth triangular number is the number of dots composing a triangle with n dots on a side, 
+//and is equal to the sum of the n natural numbers from 1 to n. 
+//This is the Triangular Number Sequence: 1, 3, 6, 10, 15, 21, 28, 36, 45.
+
+//nth number === x[(n-1)] + n
+console.log('Triangle Number')
+function triangleNumber(n){
+  if(n === 1){
+    return 1;
+  }
+  return n + triangleNumber(n-1)
+
+}
+console.log(triangleNumber(9))
