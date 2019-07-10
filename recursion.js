731a766df4e39e67a1d3a1683969820827a5aac3
Christina Labay <Christina.m.labay@gmail.com>
2019-07-09 20:25:07 -0500

    fibonacci

--- recursion.js
+++ recursion.js
@@ -61,3 +61,36 @@ function triangleNumber(n){
 
 }
 console.log(triangleNumber(9))
+
+//String Splitter
+//Write a recursive function that split a string based on a separator 
+//(similar to String.prototype.split). Don't use JS array's split function to solve this problem.
+
+console.log('String Splitter')
+
+function stringSplitter(){
+
+}
+
+//Fibonacci
+console.log('Fibonacci')
+//Write a recursive function that prints the Fibonacci sequence of a given number. 
+//The Fibonacci sequence is a series of numbers in which each number is the sum of the 2 preceding numbers. 
+//For example, the 7th Fibonacci number in a Fibonacci sequence is 13. The sequence looks as follows: 1, 1, 2, 3, 5, 8, 13.
+
+
+fibonacci=(num)=>{
+  if(num <= 1){
+    return num
+  }
+
+  return fibonacci(num-2) + fibonacci(num-1)
+}
+
+console.log(fibonacci(7))
+
+
+3
+2
+1
+1
